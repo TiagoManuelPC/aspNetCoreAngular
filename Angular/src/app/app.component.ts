db3cb71fb69582d180bb033b3bb34a268e1aa1dd
Tiago Manuel <tiagomanuelpereiracampos@hotmail.com>
2022-07-10 10:24:39 +0100

    added angular

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Something';
+export class AppComponent implements OnInit {
+	title = 'Something';
+
+	users: any;
+
+	constructor(private http: HttpClient) { }
+
+	ngOnInit() {
+		this.getUsers();
+	}
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe(res => {
+			this.users = res;
+		}, error => {
+			console.log(error);
+		});
+  }
 }
