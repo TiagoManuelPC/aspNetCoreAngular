297d1645675ddbea0471befa25245826c011bb67
Tiago Manuel <tiagomanuelpereiracampos@hotmail.com>
2022-07-19 07:42:22 +0100

    End of section 6

--- Angular/src/app/nav/nav.component.ts
+++ Angular/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -12,7 +13,8 @@ export class NavComponent implements OnInit {
     model: any = {};
 
     constructor(public accountService: AccountService,
-        private router: Router) { }
+        private router: Router,
+        private toastr: ToastrService) { }
 
     ngOnInit(): void {
     }
@@ -22,7 +24,10 @@ export class NavComponent implements OnInit {
             next: (v) => {
                 this.router.navigateByUrl('/members');
             },
-            error: (e) => console.error(e),
+            error: (e) => {
+                this.toastr.error(e.error)
+                console.error(e)
+            },
             complete: () => console.log('completed')
         })
     }
--- Angular/src/app/register/register.component.ts
+++ Angular/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -11,16 +12,22 @@ export class RegisterComponent implements OnInit {
     @Output() cancelRegister = new EventEmitter()
     model: any = {};
 
-    constructor(private accountService: AccountService) { }
+    constructor(private accountService: AccountService,
+        private toastr: ToastrService) { }
 
     ngOnInit(): void {
     }
 
     register() {
-        this.accountService.register(this.model).subscribe(res => {
-            this.cancel();
-        }, error => {
-            console.log(error);
+        this.accountService.register(this.model).subscribe( {
+            next: (v) => {
+                this.cancel();
+            },
+            error: (e) => {
+                this.toastr.error(e.error)
+                console.error(e)
+            },
+            complete: () => console.log('completed')
         })
     }
 
