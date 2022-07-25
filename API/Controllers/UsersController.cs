d446b1466dfc8f64e7ea290f5e1f51b06b4e72fc
Tiago Manuel <tiagomanuelpereiracampos@hotmail.com>
2022-07-25 08:23:20 +0100

    WIP

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,7 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
-{ 
+{
+    [Authorize]
     public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
@@ -15,13 +16,11 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
         }
 
-        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
